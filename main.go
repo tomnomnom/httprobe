b3b5a26610408c016556401fe77790e07f765f59
Tom Hudson <mail@tomnomnom.com>
2017-03-14 00:17:05 +0000

    Adds worker pool

--- main.go
+++ main.go
@@ -9,13 +9,50 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
+func worker(urls, out chan string, wg sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		url, ok := <-urls
+		if !ok {
+			return
+		}
+		if isListening(url) {
+			out <- url
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
-
 	path := flag.Arg(0)
+
+	// we send urls to check on the urls channel,
+	// but only get them on the output channel if
+	// they are accepting connections
+	urls := make(chan string)
+	output := make(chan string)
+
+	// Spin up a bunch of workers
+	var wg sync.WaitGroup
+	for i := 0; i < 40; i++ {
+		wg.Add(1)
+		go worker(urls, output, wg)
+	}
+
+	// start waiting for output straight away
+	go func() {
+		// print all of the URLs we get back
+		for url := range output {
+			fmt.Println(url)
+		}
+	}()
+
+	// Open the input file
 	f, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
@@ -26,19 +63,19 @@ func main() {
 	for sc.Scan() {
 		domain := strings.ToLower(sc.Text())
 
-		httpURL := "http://" + domain
-		httpsURL := "https://" + domain
-
-		if isListening(httpURL) {
-			fmt.Println(httpURL)
-		}
-
-		if isListening(httpsURL) {
-			fmt.Println(httpsURL)
-		}
-
+		// submit http and https versions to be checked
+		urls <- "http://" + domain
+		urls <- "https://" + domain
 	}
 
+	// once we've sent all the URLs off we can close the
+	// input channel. The workers will finish what they're
+	// doing and then call 'Done' on the WaitGroup
+	close(urls)
+
+	// Wait until all the workers have finished
+	wg.Wait()
+	close(output)
 }
 
 func isListening(url string) bool {
@@ -52,7 +89,7 @@ func isListening(url string) bool {
 	client := &http.Client{
 		Transport:     tr,
 		CheckRedirect: re,
-		Timeout:       time.Second * 5,
+		Timeout:       time.Second * 1,
 	}
 	_, err := client.Get(url)
 	if err != nil {
